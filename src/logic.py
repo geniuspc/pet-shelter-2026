817e4b03c2b94ef750c9ffb3e77715f5f55810a7
geniuspc <sensei.prihodko@gmail.com>
2026-04-03 23:17:33 +0300

    Зроблені зміни щодо класифікації #2

--- src/logic.py
+++ src/logic.py
@@ -13,7 +13,7 @@ def create_animal(db, path, contact, status):
 def get_animal(db, animal_id):
     return db.query(Animal).filter(Animal.id == animal_id).first()
 
-def update_status(db, dog_id, new_status):
+def update_status(db, animal_id, new_status):
     animal = get_animal(db, animal_id)
     if animal:
         animal.status = new_status
