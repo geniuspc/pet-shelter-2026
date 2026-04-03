4289b1540815e3a82e344005c53df2f52d48b13a
geniuspc <sensei.prihodko@gmail.com>
2026-04-03 21:49:03 +0300

    Додано платіжну систему монобанку

---
+++ src/payment.py
@@ -0,0 +1,24 @@
+import json
+import requests
+mono_url = 'https://monobank.ua'
+site_url = ''
+token = ''
+def create_payment(order_id, amount_uah):
+    paycheck = {
+        'amount': amount_uah * 100,
+        'ccy': 980,
+        'merchantPaymRefer': order_id,
+        'redirecturl': site_url,
+        'webhookurl': site_url,
+        'validaty': 7200
+    }
+    headers = {
+        'Token': token,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(mono_url, data=json.dumps(paycheck), headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {'error': response.text('Виникла помилка, спробуйте ще раз.')}
