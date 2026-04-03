e47aeffc8a3f4a0dc4237bd4b55080c50d014e18
masyk1010@gmail.com <masyk1010@gmail.com>
2026-04-03 14:18:53 +0300

    #5

--- steps/train_decorator_step.py
+++ steps/train_decorator_step.py
@@ -1,4 +1,10 @@
 from zenml import step 
 from sklearn.ensemble import IsolationForest
+import numpy as np
 
 @step
+def train_decorator_step(embedings : np.ndarray):
+
+    model = IsolationForest(n_estimators=100 , contamination = 0.05 , random_state = 42)
+
+    model.fit(embedings)
