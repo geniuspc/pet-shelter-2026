f887d52e346da4c6b4436a733051ccd8c1d81c1d
geniuspc <sensei.prihodko@gmail.com>
2026-05-20 16:19:11 +0300

    Refactoring

--- database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = create_engine('sqlite:///db.sqlite3')
-Base = declarative_base()
-SessionLocal = sessionmaker(bind=engine)
