3eacb68ebdfff2b160012dd2208231b4a5b373ef
geniuspc <sensei.prihodko@gmail.com>
2026-05-20 16:19:48 +0300

    Refactoring

--- init_db.py
+++
@@ -1,6 +0,0 @@
-from src.database import engine, Base
-from src.model import Animal, Location, Match
-
-print("Створюємо таблиці в базі даних...")
-Base.metadata.create_all(bind=engine)
-print("Готово! Таблиці створені успішно. 🚀")
